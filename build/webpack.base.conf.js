d0560765c372ec2f9a912c0c5317ada2b8cea0b5
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-15 13:37:40 +0100

    :lipstick: style: remove unused font formats

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,7 +75,7 @@ module.exports = {
       },
       {
           test: /\.scss$/,
-          loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+          loader: 'sass-loader'
       }
     ]
   }
