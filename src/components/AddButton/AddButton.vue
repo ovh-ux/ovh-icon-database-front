9035a6934a79f22a839c101afa495f4a5064dba8
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-18 14:38:24 +0100

    :lipstick: style(addButton): remove xlink in <use>

--- src/components/AddButton/AddButton.vue
+++ src/components/AddButton/AddButton.vue
@@ -5,7 +5,7 @@
           <span class="addButton__icon-container">
             <svg xmlns="http://www.w3.org/2000/svg"
                  class="addButton__icon">
-                <use xlink:href="./src/assets/icons.svg#plus_20px" />
+                <use href="./src/assets/icons.svg#plus_20px" />
             </svg>
           </span>
           <span class="addButton__label">Add icon</span>
