b72c036f7d166dfd7419bda2addb60f12d9c4b5e
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-18 11:34:24 +0100

    chore: remove loginPage

--- src/components/LoginPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <section>
-        <h2>Login</h2>
-    </section>
-</template>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-</style>
