0c0240a1f5143b2628973e3a244f7075d5837024
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-18 14:20:26 +0100

    :lipstick: style(addButton): fix PR
    remove methods and svg in a separate file

--- src/components/AddButton/AddButton.vue
+++ src/components/AddButton/AddButton.vue
@@ -4,10 +4,8 @@
           to="/upload">
           <span class="addButton__icon-container">
             <svg xmlns="http://www.w3.org/2000/svg"
-                 class="addButton__icon"
-                 viewBox="0 0 20 20">
-                <line class="cls-1" x1="10" y1="19.5" x2="10" y2="0.5"/>
-                <line class="cls-1" x1="0.5" y1="10" x2="19.5" y2="10"/>
+                 class="addButton__icon">
+                <use xlink:href="./src/assets/icons.svg#plus_20px" />
             </svg>
           </span>
           <span class="addButton__label">Add icon</span>
@@ -17,12 +15,7 @@
 
 <script>
     export default {
-        name: 'AddButton',
-        methods: {
-            addIcon(icon) {
-                console.log(icon);
-            }
-        }
+        name: 'AddButton'
     };
 </script>
 
