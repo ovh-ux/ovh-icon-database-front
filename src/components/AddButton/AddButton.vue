c1cec21893c87bddf8e22e331e1c1592b1a18a63
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-18 10:40:37 +0100

    :lipstick: style(addButton): add button icon and hover styles

--- src/components/AddButton/AddButton.vue
+++ src/components/AddButton/AddButton.vue
@@ -1,7 +1,14 @@
 <template>
     <li class="addButton icon-list__item">
         <router-link to="/upload"></router-link>
-          <span class="addButton__icon"></span>
+          <span class="addButton__icon-container">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 class="addButton__icon"
+                 viewBox="0 0 20 20">
+                <line class="cls-1" x1="10" y1="19.5" x2="10" y2="0.5"/>
+                <line class="cls-1" x1="0.5" y1="10" x2="19.5" y2="10"/>
+            </svg>
+          </span>
           <span class="addButton__label">Add icon</span>
         </a>
     </li>
