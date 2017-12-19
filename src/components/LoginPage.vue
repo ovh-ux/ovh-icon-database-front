bcef18f0ae329b249079fe406f28ea1eb8e37412
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-19 09:50:59 +0100

    :sparkles: feat(login): create login page components

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,7 +1,20 @@
 <template>
     <section>
+      <h2>Log In</h2>
+      <LoginForm></LoginForm>
     </section>
 </template>
+
+<script>
+import LoginForm from '@/components/LoginForm/LoginForm';
+
+export default {
+    name: 'LoginPage',
+    components: {
+        LoginForm
+    }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
---
+++ src/components/loginForm/loginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form action="">
+    <label for="username">Username</label>
+    <input id="username"
+      type="text">
+    <label for="password">Password</label>
+    <input id="password"
+      type="text">
+    <button type="submit" name="button">Login</button>
+    <a href="">Forgot your password ?</a>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
