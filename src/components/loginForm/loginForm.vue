3ef696a1f0fe13e9f115991a14557c5ed3f08d4a
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-19 16:53:37 +0100

    :lipstick: style: form basic focus styles

--- src/components/loginForm/loginForm.vue
+++ src/components/loginForm/loginForm.vue
@@ -1,22 +1,28 @@
 <template>
   <form action="" class="login-form">
-    <label for="username"
-      class="input-text-label">Username</label>
-    <input id="username"
-      name="username"
-      type="text"
-      class="input-text login-form__input">
-    <label for="password"
-      class="input-text-label">Password</label>
-    <input id="password"
-      name="password"
-      type="text"
-      class="input-text login-form__input">
-    <button class="btn btn--primary login-form__btn"
-      type="submit"
-      name="button">Login</button>
-    <a href=""
-      class="btn btn--link login-form__btn">Forgot your password ?</a>
+    <div class="input-text-row">
+      <input id="username"
+        name="username"
+        type="text"
+        class="input-text login-form__input">
+      <label for="username"
+        class="input-text-label">Username</label>
+    </div>
+    <div class="input-text-row">
+      <input id="password"
+        name="password"
+        type="text"
+        class="input-text login-form__input">
+      <label for="password"
+        class="input-text-label">Password</label>
+    </div>
+    <div class="login-form__footer">
+      <button class="btn btn--primary login-form__btn"
+        type="submit"
+        name="button">Login</button>
+      <a href=""
+        class="btn btn--link login-form__btn login-form__btn--password">Forgot your password ?</a>
+    </div>
   </form>
 </template>
 
