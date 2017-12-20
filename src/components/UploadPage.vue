1c2ab3fa2bc87bca23bbde5f98c1b433dd5a55d3
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-20 16:15:24 +0100

    :sparkles: feat(upload page): base settings for vue-clip

--- src/components/UploadPage.vue
+++ src/components/UploadPage.vue
@@ -1,4 +1,22 @@
 <template>
+  <vue-clip :options="options">
+    <template slot="clip-uploader-action">
+      <div>
+        <div class="dz-message">
+          <p>Drop your icons in .svg format in this area
+            <br>or
+          </p>
+          <button type="button" class="btn btn--primary">Upload files</button>
+        </div>
+      </div>
+    </template>
+    <template slot="clip-uploader-body"
+      scope="props">
+      <div v-for="file in props.files">
+        {{ file.name }}
+      </div>
+    </template>
+  </vue-clip>
 </template>
 
 <script>
@@ -6,6 +24,13 @@
 export default {
     name: 'Uploadpage',
     components: {
+    },
+    data: () => {
+      return {
+        options: {
+          url: '/upload'
+        }
+      }
     }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueClip from 'vue-clip'
 import HomePage from '@/components/HomePage'
 import UploadPage from '@/components/UploadPage'
 
 Vue.use(Router)
+Vue.use(VueClip)
 
 export default new Router({
   routes: [
