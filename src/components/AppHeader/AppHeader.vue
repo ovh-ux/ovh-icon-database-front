7ab90102af35b0b4b9d587f674f51cadc8259827
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-12-20 10:58:07 +0100

    :ambulance: fix(header): fix fix header's links

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -1,11 +1,14 @@
 <template>
     <header class="header">
       <h1 class="header__title">
-          <img class="header__title-logo" src="../../assets/logo.svg" alt="OVH group logo">
-          <span class="header__title-app-name">
-            UI Kit
-            <span class="header__title-app-name--colored">{{ title }}</span>
-          </span>
+          <router-link class="header__title-link"
+            to="/">
+            <img class="header__title-logo" src="../../assets/logo.svg" alt="OVH group logo">
+            <span class="header__title-app-name">
+              UI Kit
+              <span class="header__title-app-name--colored">{{ title }}</span>
+            </span>
+          </router-link>
       </h1>
       <LoginButton></LoginButton>
     </header>
--- src/components/AppHeader/LoginButton/LoginButton.vue
+++ src/components/AppHeader/LoginButton/LoginButton.vue
@@ -1,6 +1,5 @@
 <template>
     <router-link class="loginButton"
-            type="button"
             to="/login">
       <svg xmlns="http://www.w3.org/2000/svg"
            viewBox="0 0 16 16"
