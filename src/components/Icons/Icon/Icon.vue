872422c5594d79f44850b1f31b5883f1fcf78502
kevin.bonduelle <kevin.bonduelle@corp.ovh.com>
2018-01-03 17:15:10 +0100

    :sparkles: feat(homepage): add className for selected icon

--- src/components/Icons/Icon/Icon.vue
+++ src/components/Icons/Icon/Icon.vue
@@ -1,5 +1,6 @@
 <template>
-    <li class="icon icon-list__item" @click="selectIcon(icon)">
+    <li class="icon icon-list__item" @click="selectIcon(icon)"
+        v-bind:class="{ 'icon-list__item--selected': icon.selected }">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon__visual" v-html="icon.svg"></svg>
         <span class="icon__name">{{icon.name}}</span>
     </li>
