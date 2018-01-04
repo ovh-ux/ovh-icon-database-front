d31646686fee91078dd05fee1e2a3e786a9036e1
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-04 15:51:29 +0100

    :sparkles: feat(homepage): create component files for homepage loader

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,15 +2,18 @@
     <section>
         <page-title :pagename="'OVH Icon Database'"></page-title>
         <icons-filter></icons-filter>
-        <icons></icons>
+        <icons v-if="!loading"></icons>
+        <icons-loading v-else></icons-loading>
     </section>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 import PageTitle from '@/components/PageTitle/PageTitle'
 import IconsFilter from '@/components/IconsFilter/IconsFilter'
-import { mapActions } from 'vuex'
 import Icons from '@/components/Icons/Icons'
+import IconsLoading from '@/components/Icons/IconsLoading/IconsLoading'
 
 export default {
     name: 'HomePage',
@@ -31,7 +34,8 @@ export default {
     components: {
         Icons,
         PageTitle,
-        IconsFilter
+        IconsFilter,
+        IconsLoading
     }
 }
 </script>
