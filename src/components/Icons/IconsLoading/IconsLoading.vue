30f71bdb31d9b3e669063922b05f6075df748795
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-05 11:29:40 +0100

    :lipstick: style(icons): set animation for icons loading

--- src/components/Icons/IconsLoading/IconsLoading.vue
+++ src/components/Icons/IconsLoading/IconsLoading.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="icons-loading"></div>
+    <ul class="icons-loading">
+      <li class="icons-loading__item">
+        <span class="icons-loading__placeholder-visual"></span>
+        <span class="icons-loading__placeholder-name"></span>
+      </li>
+      <li class="icons-loading__item">
+        <span class="icons-loading__placeholder-visual"></span>
+        <span class="icons-loading__placeholder-name"></span>
+      </li>
+      <li class="icons-loading__item">
+        <span class="icons-loading__placeholder-visual"></span>
+        <span class="icons-loading__placeholder-name"></span>
+      </li>
+    </ul>
 </template>
 
 <script>
@@ -7,3 +20,7 @@ export default {
     name: 'IconsLoading'
 }
 </script>
+
+<style lang="scss" scoped>
+@import "./IconsLoading.scss";
+</style>
