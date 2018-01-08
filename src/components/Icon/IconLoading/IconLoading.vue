dd211a55a3b42c6bdeb77f88f7152d37dbaac06a
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-08 10:37:02 +0100

    :tractor: refactor(icons loading): use v-for

--- src/components/Icon/IconLoading/IconLoading.vue
+++ src/components/Icon/IconLoading/IconLoading.vue
@@ -1,14 +1,6 @@
 <template>
     <ul class="icons-loading">
-      <li class="icons-loading__item">
-        <span class="icons-loading__placeholder-visual"></span>
-        <span class="icons-loading__placeholder-name"></span>
-      </li>
-      <li class="icons-loading__item">
-        <span class="icons-loading__placeholder-visual"></span>
-        <span class="icons-loading__placeholder-name"></span>
-      </li>
-      <li class="icons-loading__item">
+      <li class="icons-loading__item" v-for="n in 3">
         <span class="icons-loading__placeholder-visual"></span>
         <span class="icons-loading__placeholder-name"></span>
       </li>
