0f13754bf7425e3ebab49884bb53b6d07cedad55
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-09 17:42:07 +0100

    :sparkles: feat: filter icons by name

--- src/store/StoreIcons.js
+++ src/store/StoreIcons.js
@@ -6,7 +6,8 @@ const baseUrl = `${process.env.OSS_URL}/v1/${process.env.OSS_AUTH}/${process.env
 
 export const moduleIcons = {
     state: {
-        icons: []
+        icons: [],
+        searchName: ''
     },
     getters: {
 
@@ -20,6 +21,14 @@ export const moduleIcons = {
 
       iconsCount: state => {
         return state.icons.length;
+      },
+
+      getSearchName: state => {
+        return state.searchName;
+      },
+
+      filteredIcons: state => {
+        return state.icons.filter(icon => icon.name.match(state.searchName));
       }
 
     },
@@ -29,9 +38,13 @@ export const moduleIcons = {
             state.icons = icons;
         },
 
+        setSearchName(state, value) {
+            state.searchName = value;
+        },
+
         selectIcon(state, icon) {
             var currentIconIdx = state.icons.findIndex((elm) => {
-                return elm.id === icon.id;
+                return elm.url === icon.url;
             });
 
             Vue.set(
@@ -45,6 +58,10 @@ export const moduleIcons = {
 
         selectIcon({ commit }, icon) {
             commit('selectIcon', icon);
+        },
+
+        setSearchName({ commit }, value) {
+          commit('setSearchName', value)
         },
 
         fetchIcons({ commit }) {
