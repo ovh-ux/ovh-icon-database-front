c58dba45bee7073a744eb474b021736b9e0bd7f4
Kévin Bonduelle <kevin.bonduelle@corp.ovh.com>
2018-01-09 17:29:03 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,11 +6,6 @@ const path = require('path')
 
 module.exports = {
 
-  OSS_URL: JSON.stringify(process.env.OSS_URL || ''),
-  OSS_AUTH: JSON.stringify(process.env.OSS_AUTH || ''),
-  OSS_CONTAINER: JSON.stringify(process.env.OSS_CONTAINER || ''),
-  API_URL: JSON.stringify(process.env.API_URL || ''),
-
   dev: {
 
     // Paths
