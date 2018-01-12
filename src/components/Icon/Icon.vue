aa8f711f4fe9ab21d92a5b2d02acb45c3299b728
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-12 11:07:13 +0100

    :lipstick: style(icon): icon visual style fix

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -5,7 +5,7 @@
              class="icon"
              @click="selectIcon(icon)"
              v-bind:class="{ 'icon--selected': icon.selected }">
-             <div class="icon__visual" v-html="icon.raw"></div>
+             <span class="icon__visual" v-html="icon.raw"></span>
              <span class="icon__name">{{icon.name}}</span>
           </button>
       </li>
