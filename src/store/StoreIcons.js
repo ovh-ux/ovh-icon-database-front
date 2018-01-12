0dc51401ac096acc484c0c7b71d0e4d19fdfec1e
kevin.bonduelle <kevin.bonduelle@corp.ovh.com>
2018-01-12 10:27:44 +0100

    :tractor: refactor: remove log

--- src/store/StoreIcons.js
+++ src/store/StoreIcons.js
@@ -4,8 +4,6 @@ import { api } from '../utils/request';
 
 const baseUrl = `${process.env.OSS_URL}/v1/${process.env.OSS_AUTH}/${process.env.OSS_CONTAINER}`;
 
-console.log("===>", process.env);
-
 export const moduleIcons = {
     state: {
         icons: [],
