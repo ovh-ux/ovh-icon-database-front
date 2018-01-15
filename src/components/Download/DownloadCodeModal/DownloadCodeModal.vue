11ebc96cc5777ee01c833d0a5905246f8d32c4e5
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-15 15:34:22 +0100

    :sparkles: feat(download): html for svg code modal

--- src/components/Download/DownloadCodeModal/DownloadCodeModal.vue
+++ src/components/Download/DownloadCodeModal/DownloadCodeModal.vue
@@ -1,5 +1,22 @@
 <template>
-
+  <div class="download-code-modal__overlay">
+    <div class="download-code-modal">
+      <h2 class="download-code-modal__title">
+        SVGs source code
+      </h2>
+      <pre class="download-code-modal__svg-code">
+        lekfijg
+      </pre>
+      <button type="button"
+        class="btn btn--close download-code-modal__close">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             class="btn__icon">
+             <title>Close modal</title>
+            <use href="#cross_16px" />
+        </svg>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
