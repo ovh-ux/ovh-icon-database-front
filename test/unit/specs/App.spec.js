55af7a216a16bd93312a520a43595c2cffbb6e88
kevin.bonduelle <kevin.bonduelle@corp.ovh.com>
2018-01-17 16:23:56 +0100

    :tractor: refactor: add test

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -5,22 +5,18 @@ import store from '../../../src/store/index';
 
 Vue.use(Vuex);
 
-
 import HomePage from '@/components/HomePage';
 
 describe('HomePage.vue', () => {
 
-  it('should render correct contents of title', () => {
-    const Constructor = Vue.extend(HomePage);
-    const vm = new Constructor({ store }).$mount();
-    expect(vm.$el.querySelector('.page-title').textContent)
-      .to.equal('OVH Icon Database');
-  });
+  const Constructor = Vue.extend(HomePage);
+  const vm = new Constructor({ store }).$mount();
 
   it('should render correct contents of title', () => {
-    const Constructor = Vue.extend(HomePage);
-    const vm = new Constructor({ store }).$mount();
-    expect(vm.$el.querySelector('.page-title').textContent)
-      .to.equal('OVH Icon Database');
+    expect(vm.$el.querySelector('.page-title').textContent).to.equal('OVH Icon Database');
+  });
+
+  it('Search should be empty', () => {
+    expect(vm.$el.querySelector('.icons-filter__search-field').value).to.equal("");
   });
 });
