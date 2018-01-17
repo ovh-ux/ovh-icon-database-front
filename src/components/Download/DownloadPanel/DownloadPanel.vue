b12ba0f08c1c1a4f70deb4b5ef755c8b40c27b36
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-17 15:18:48 +0100

    :tractor: refactor(download): html structure

--- src/components/Download/DownloadPanel/DownloadPanel.vue
+++ src/components/Download/DownloadPanel/DownloadPanel.vue
@@ -1,52 +1,68 @@
 <template>
   <div>
-    <div v-if="modalState" class="download-panel__overlay"></div>
+    <transition name="download-panel-overlay">
+      <div v-if="modalState" class="download-panel__overlay"></div>
+    </transition>
 
-    <div class="download-panel" :class="{'download-panel--maximized': modalState}">
-      <div v-if="!modalState" class="download-panel__header">
-        {{ selectedIcons.length }} icon(s) selected
-      </div>
+    <div class="download-panel"
+      :class="[modalState ? 'download-panel--maximized': 'download-panel--minimized']">
 
-      <div v-else class="download-panel__header">
-        <button type="button"
-          class="btn btn--close download-panel__back"
-          @click="toggleModal(false)">
-          <svg xmlns="http://www.w3.org/2000/svg"
-               class="btn__icon">
-               <title>Close modal</title>
-              <use href="#leftarrow_16px" />
-          </svg>
-        </button>
-        <h2 class="download-panel__title">SVGs source code</h2>
-      </div>
+      <transition mode="in-out">
+        <div class="download-panel__content" v-if="!modalState">
+          <div class="download-panel__title">
+            {{ selectedIcons.length }} icon(s) selected
+          </div>
+          <div>
+            <button class="btn btn--default download-panel__button"
+              type="button"
+              @click="toggleModal()">
+              Get code
+            </button>
+            <button class="btn btn--primary download-panel__button"
+              @click="downloadSVGs"
+              type="button">
+              Download svg
+            </button>
+          </div>
+        </div>
 
-      <div v-if="modalState" class="download-panel__body">
-        <download-get-code
-          v-for="icon in selectedIcons"
-          :key="icon.id"
-          :icon="icon">
-        </download-get-code>
-      </div>
+        <div class="download-panel__content" v-else>
+          <div class="download-panel__header">
+            <button type="button"
+              class="btn btn--close download-panel__back"
+              @click="toggleModal(false)">
+              <svg xmlns="http://www.w3.org/2000/svg"
+                   class="btn__icon">
+                   <title>Close modal</title>
+                  <use href="#leftarrow_16px" />
+              </svg>
+            </button>
+            <h2 class="download-panel__title">SVGs source code</h2>
+          </div>
 
-      <div class="download-panel__footer">
-        <button v-if="!modalState"
-          class="btn btn--default download-panel__button"
-          type="button"
-          @click="toggleModal()">
-          Get code
-        </button>
-        <button class="btn btn--primary download-panel__button"
-          @click="downloadSVGs"
-          type="button">
-          Download svg
-        </button>
-      </div>
+          <div class="download-panel__body">
+            <download-get-code
+              v-for="icon in selectedIcons"
+              :key="icon.id"
+              :icon="icon">
+            </download-get-code>
+          </div>
+
+          <div class="download-panel__footer">
+            <button class="btn btn--primary download-panel__button"
+              @click="downloadSVGs"
+              type="button">
+              Download svg
+            </button>
+          </div>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex'
 
 import DownloadGetCode from '@/components/Download/DownloadGetCode/DownloadGetCode'
 
@@ -54,6 +70,7 @@ export default {
     name: 'DownloadPanel',
     computed: mapGetters({
       'selectedIcons': 'icons/selected',
+      'hasSelectedIcon': 'icons/hasSelected',
       'modalState': 'downloadModal/isOpen'
     }),
     methods: mapActions({
