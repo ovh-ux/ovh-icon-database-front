53417f4a567cab6b7d10c14a2c152c5f45baa162
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-17 15:52:21 +0100

    :lipstick: style(download): transitions

--- src/components/Download/DownloadPanel/DownloadPanel.vue
+++ src/components/Download/DownloadPanel/DownloadPanel.vue
@@ -4,60 +4,65 @@
       <div v-if="modalState" class="download-panel__overlay"></div>
     </transition>
 
-    <div class="download-panel"
-      :class="[modalState ? 'download-panel--maximized': 'download-panel--minimized']">
+    <transition name="download-panel" mode="out-in">
+      <div class="download-panel download-panel--minimized"
+        v-if="!modalState"
+        key="minimized">
+        <div class="download-panel__title">
+          {{ selectedIcons.length }} icon(s) selected
+        </div>
+        <div>
+          <button class="btn btn--default download-panel__button"
+            type="button"
+            @click="toggleModal()">
+            Get code
+          </button>
+          <button class="btn btn--primary download-panel__button"
+            @click="downloadSVGs"
+            type="button">
+            Download svg
+          </button>
+        </div>
+      </div>
 
-      <transition mode="in-out">
-        <div class="download-panel__content" v-if="!modalState">
-          <div class="download-panel__title">
-            {{ selectedIcons.length }} icon(s) selected
-          </div>
-          <div>
-            <button class="btn btn--default download-panel__button"
-              type="button"
-              @click="toggleModal()">
-              Get code
-            </button>
-            <button class="btn btn--primary download-panel__button"
-              @click="downloadSVGs"
-              type="button">
-              Download svg
-            </button>
-          </div>
+      <div class="download-panel download-panel--maximized"
+        v-else
+        key="maximized">
+        <div class="download-panel__header">
+          <button type="button"
+            class="btn btn--close download-panel__back"
+            @click="toggleModal(false)">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 class="btn__icon">
+                 <title>Close modal</title>
+                <use href="#leftarrow_16px" />
+            </svg>
+          </button>
+          <h2 class="download-panel__title">SVGs source code</h2>
         </div>
 
-        <div class="download-panel__content" v-else>
-          <div class="download-panel__header">
-            <button type="button"
-              class="btn btn--close download-panel__back"
-              @click="toggleModal(false)">
-              <svg xmlns="http://www.w3.org/2000/svg"
-                   class="btn__icon">
-                   <title>Close modal</title>
-                  <use href="#leftarrow_16px" />
-              </svg>
-            </button>
-            <h2 class="download-panel__title">SVGs source code</h2>
-          </div>
-
-          <div class="download-panel__body">
-            <download-get-code
-              v-for="icon in selectedIcons"
-              :key="icon.id"
-              :icon="icon">
-            </download-get-code>
-          </div>
-
-          <div class="download-panel__footer">
-            <button class="btn btn--primary download-panel__button"
-              @click="downloadSVGs"
-              type="button">
-              Download svg
-            </button>
-          </div>
+        <div class="download-panel__body">
+          <download-get-code
+            v-for="icon in selectedIcons"
+            :key="icon.id"
+            :icon="icon">
+          </download-get-code>
         </div>
-      </transition>
-    </div>
+
+        <div class="download-panel__footer">
+          <button class="btn btn--default download-panel__button"
+            type="button"
+            @click="toggleModal(false)">
+            Hide code
+          </button>
+          <button class="btn btn--primary download-panel__button"
+            @click="downloadSVGs"
+            type="button">
+            Download svg
+          </button>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
