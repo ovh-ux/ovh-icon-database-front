cae8580e63ff1aee3a940af99b07b7e8a235646e
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-01-24 10:01:30 +0100

    :ambulance: fix: fix remove spaces in classes names

--- src/store/Icons/actions.js
+++ src/store/Icons/actions.js
@@ -18,7 +18,6 @@ export default {
       let icons = response.data.data;
       icons.forEach(icon => {
         icon.name = icon.name.replace('.svg', '');
-        icon.name = icon.name.replace('/\s/g', '');
 
         let div = document.createElement('div');
         div.innerHTML = icon.raw;
@@ -39,7 +38,7 @@ export default {
         }
         let classes = Object.keys(hash);
         for (let i = 0; i < classes.length; i++) {
-          div.innerHTML = div.innerHTML.replace(new RegExp(classes[i], 'g'), icon.name + '--' + classes[i]);
+          div.innerHTML = div.innerHTML.replace(new RegExp(classes[i], 'g'), icon.name.replace(/\s/g, '-') + '--' + classes[i]);
         }
         icon.raw = div.innerHTML;
       });
